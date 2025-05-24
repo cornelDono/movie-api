ac82554bc2586485ef768f6c904f3e39d4b3d1b5
Andrey <strenkovsky@gmail.com>
2025-05-25 01:13:04 +0400

    feat(docker): Added postgres docker container

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,19 +1,30 @@
 package main
 
 import (
-	"flag"
+	"context"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/joho/godotenv/autoload"
 )
 
 const version = "1.0.0"
 
+type potsgresConfig struct {
+	user     string
+	password string
+	host     string
+	port     string
+	db_name  string
+}
 type config struct {
 	port int
 	env  string
+	db   potsgresConfig
 }
 
 type application struct {
@@ -22,13 +33,18 @@ type application struct {
 }
 
 func main() {
-	var cfg config
-
-	flag.IntVar(&cfg.port, "port", 4000, "API Server Port")
-	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
-	flag.Parse()
-
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	cfg, err := loadConfig()
+	if err != nil {
+		logger.Fatal(err)
+	}
+
+	dbpool, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer dbpool.Close()
 
 	app := application{
 		config: cfg,
@@ -44,6 +60,41 @@ func main() {
 	}
 
 	logger.Printf("starting %s severs on %s", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
-	logger.Fatal(err)
+	listen_err := srv.ListenAndServe()
+	logger.Fatal(listen_err)
+}
+
+func openDB(cfg config) (*pgxpool.Pool, error) {
+	dsn := fmt.Sprintf("postgres://%s:%s@%s:%s/%s", cfg.db.user, cfg.db.password, cfg.db.host, cfg.db.port, cfg.db.db_name)
+	dbpool, err := pgxpool.New(context.Background(), dsn)
+	dbpool.Config().MaxConnIdleTime, _ = time.ParseDuration("15m")
+	dbpool.Config().MaxConns = 50
+
+	if err != nil {
+		return nil, err
+	}
+
+	var greeting string
+	err = dbpool.QueryRow(context.Background(), "select 'Hello, world!'").Scan(&greeting)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbpool, nil
+}
+
+func loadConfig() (config, error) {
+	cfg := config{
+		port: 4000,
+		env:  "development",
+		db: potsgresConfig{
+			user:     os.Getenv("DB_USER"),
+			password: os.Getenv("DB_PASSWORD"),
+			host:     os.Getenv("DB_HOST"),
+			port:     os.Getenv("DB_PORT"),
+			db_name:  os.Getenv("DB_NAME"),
+		},
+	}
+
+	return cfg, nil
 }
